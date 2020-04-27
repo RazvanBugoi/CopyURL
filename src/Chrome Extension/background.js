a3d74eece1e7c2bb81034123a134a2e807eccd6e
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-04-27 16:41:07 +0100

    updates

--- src/Chrome Extension/background.js
+++ src/Chrome Extension/background.js
@@ -1,4 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   let url = tab.url;
-
+  let link = document.createElement('textarea');
+  document.body.appendChild(link);
+  link = url;
+  link.select();
+  document.execCommand('copy', link);
   });
